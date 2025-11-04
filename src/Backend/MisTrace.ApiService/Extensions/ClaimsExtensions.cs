8d0004c85777ef80a872c67ad40b4408149cd23b
Kevin Becerra <kbecerra774@gmail.com>
2025-11-03 20:25:55 -0600

    fixed claims parsing

--- src/Backend/MisTrace.ApiService/Extensions/ClaimsExtensions.cs
+++ src/Backend/MisTrace.ApiService/Extensions/ClaimsExtensions.cs
@@ -1,6 +1,4 @@
-using System;
 using System.Security.Claims;
-using System.Security.Cryptography.X509Certificates;
 using MisTrace.Application.DTOs;
 
 namespace MisTrace.ApiService.Extensions;
@@ -9,7 +7,8 @@ public static class ClaimsExtensions
 {
     public static UserDto BuildUserFromClaims(ClaimsPrincipal user)
     {
-        string subClaim = user.FindFirstValue("sub")
+        string subClaim = user.FindFirstValue(ClaimTypes.NameIdentifier)
+            ?? user.FindFirstValue("sub")
             ?? throw new UnauthorizedAccessException("InvalidClaims");
 
         Guid sub = new(subClaim);
@@ -17,10 +16,7 @@ public static class ClaimsExtensions
         string orgClaim = user.FindFirstValue("org")
             ?? throw new UnauthorizedAccessException("InvalidClaims");
 
-        if (!int.TryParse(orgClaim, out int orgId))
-            throw new UnauthorizedAccessException("InvalidClaims");
-
-        if (orgId < 1)
+        if (!int.TryParse(orgClaim, out int orgId) || orgId < 1)
             throw new UnauthorizedAccessException("InvalidClaims");
 
         return new UserDto
@@ -29,4 +25,5 @@ public static class ClaimsExtensions
             OrganizationId = orgId
         };
     }
+
 }
